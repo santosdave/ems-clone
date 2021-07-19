1d0be8a95a435108acb1025bb32371097bbd455c
santos dave <santosdave86@gmail.com>
2021-07-19 16:05:14 +0300

    first commit

---
+++ components/navigation/Header.js
@@ -0,0 +1,122 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+/* import {signOut, useSession} from 'next-auth/client'; */
+import {useState} from "react";
+import Modal from "@material-tailwind/react/Modal";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import Modalfooter from "@material-tailwind/react/Modalfooter";
+import Dropdown from "@material-tailwind/react/Dropdown"
+import DropdownItem from "@material-tailwind/react/DropdownItem"
+import DropdownLink from "@material-tailwind/react/DropdownLink"
+function Header() {
+    /* const [session]=useSession(); */
+    const [showModal, setShowModal]= useState(false)
+    const modal=(
+        <Modal
+        size="sm"
+        active={showModal}
+        toggler={()=>setShowModal(false)}
+        >
+        <ModalBody>
+            <div className="flex flex-col items-center">
+                <img 
+                onClick={()=>setShowModal(true)}
+                loading="lazy"
+                className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                /* src={session?.user?.image} */
+                alt=""
+                />
+                <p className="font-semibold text-sm text-gray-700">{"Santos Dave"}</p>
+                <p className="text-sm text-gray-700">{"santosdave86@gmail.com"}</p>
+            </div>
+            
+        </ModalBody>
+        <Modalfooter>
+            <Button
+            color="blue"
+            buttonType="link"
+            onClick={(e)=>setShowModal(false)}
+            ripple={"dark"}
+            >
+            Cancel
+            </Button>
+            <Button
+            color="blue"
+            /* onClick={signOut} */
+            ripple={"light"}
+            >
+            signOut
+            </Button>
+        </Modalfooter>
+    </Modal>
+    )
+    
+    return(
+        <header className="sticky top-0 z-50 flex items-center   px-4 py-2 shadow-md bg-black ">
+            
+            <Dropdown
+            color="lightBlack"
+            placement="bottom-start"
+            buttonText="MENU"
+            buttonType="filled"
+            size="lg"
+            rounded={false}
+            block={false}
+            ripple="light"
+        >
+                <DropdownItem color="lightBlue" ripple="light">
+                    Action
+                </DropdownItem>
+                <DropdownLink
+                    href="#"
+                    color={"gray"}
+                    ripple="light"
+                    onClick={(e) => e.preventDefault()}
+                >
+                    Another Action
+                </DropdownLink>
+                <DropdownItem color="lightBlue" ripple="light">
+                    Something else
+                </DropdownItem>
+        </Dropdown>
+           
+            <img className="cursor-pointer h-12 w-35 rounded-lg ml-2 " alt="" loading="lazy" src="https://widetechnologies.co.ke/wp/wp-content/uploads/2021/06/wtl0go4b_white.png"/>
+            
+            <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:shadow-md">
+                <Icon name="search" size="3xl" color="gray"/>
+                <input type="text" placeholder="Search" className="flex-grow px-5 text-base outline-none bg-transparent border-0"/>
+            </div>
+            <Button
+            color="gray"
+            buttonType="outline"
+            rounded={true}
+            iconOnly={true}
+            ripple={"dark"}
+            className="sm:hidden md:inline-flex h-20 w-20 border-0"
+            >
+                <Icon name="apps" size="3xl"/>
+            </Button>
+            <Button
+            color="gray"
+            buttonType="outline"
+            rounded={true}
+            iconOnly={true}
+            ripple={"dark"}
+            className="sm:hidden md:inline-flex h-20 w-20 border-0"
+            >
+                <Icon name="notifications" size="3xl"/>
+            </Button>
+            <img 
+            onClick={()=>setShowModal(true)}
+            loading="lazy"
+            className="cursor-pointer h-12 w-12 rounded-full ml-2"
+            /* src={session?.user?.image} */
+            alt=""
+            />
+            {modal}
+        </header>
+        )
+    
+}
+
+export default Header
---
+++ components/navigation/LeftSidebar.js
@@ -0,0 +1,285 @@
+import Icon from '@material-tailwind/react/Icon';
+import H6 from '@material-tailwind/react/Heading6';
+function LeftSidebar() {
+    return (
+        <>
+        
+        
+        <div
+            className={`h-screen fixed  md:left-0 overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-xl bg-white w-64 z-10 py-4 px-6 transition-all duration-300`}
+        >
+            <div className="  ">
+                <a
+                    href="https://material-tailwind.com?ref=mtd"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="mt-2 text-center w-full inline-block"
+                >
+                    <H6 className="" color="gray"><Icon name="account_circle" size="3xl" />User Profile</H6>
+                </a>
+            </div>
+            <div className="flex-col items-stretch min-h-full flex-nowrap px-0 relative">
+                
+                <div className="flex flex-col">
+                    < H6 color="gray">Main</H6>
+                    <ul className="flex-col min-w-full flex list-none">
+                        <li className="px-4 rounded-lg mb-2  hover:bg-gray-300">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/login"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="fingerprint" size="2xl" />
+                                    Email
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/register"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="list_alt" size="2xl" />
+                                    Compose
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Inbox
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Sent
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Group Chat
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Calendar
+                                </a>
+                            </li>
+                    </ul>
+
+                    < H6 color="gray">Staff</H6>
+                    <ul className="flex-col min-w-full flex list-none">
+                        
+                        
+                        <li className="px-4 rounded-lg mb-2  hover:bg-gray-300">
+                            <a
+                                href="https://demos.creative-tim.com/material-tailwind-kit-react/#/login"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="flex items-center gap-4 text-sm font-semibold py-3"
+                            >
+                                <Icon name="fingerprint" size="2xl" />
+                                Staff List
+                            </a>
+                        </li>
+                        <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                            <a
+                                href="https://demos.creative-tim.com/material-tailwind-kit-react/#/register"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="flex items-center gap-4 text-sm font-semibold py-3"
+                            >
+                                <Icon name="list_alt" size="2xl" />
+                                Department
+                            </a>
+                        </li>
+                        <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                            <a
+                                href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="flex items-center gap-4 text-sm font-semibold py-3"
+                            >
+                                <Icon name="web" size="2xl" />
+                                Leave Application
+                            </a>
+                        </li>
+                        <li className="px-4 rounded-lg mb-2 hover:bg-gray-300">
+                            <a
+                                href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="flex items-center gap-4 text-sm font-semibold py-3"
+                            >
+                                <Icon name="account_circle" size="2xl" />
+                                Allowance & Benefits
+                            </a>
+                        </li>
+                        <li className="px-4 rounded-lg mb-2  hover:bg-gray-300">
+                            <a
+                                href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="flex items-center gap-4 text-sm font-semibold py-3"
+                            >
+                                <Icon name="account_circle" size="2xl" />
+                                Pay Advice
+                            </a>
+                        </li>
+                        <H6 color="gray">Customer</H6>
+                        <div data-trigger="Customer" className="">
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Customer List
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="account_circle" size="2xl" />
+                                    Quotation
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2  hover:bg-gray-300">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="account_circle" size="2xl" />
+                                    Purchase Order
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="account_circle" size="2xl" />
+                                    Delivery Order
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2  hover:bg-gray-300">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/profile"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="account_circle" size="2xl" />
+                                    Invoice
+                                </a>
+                            </li>
+                        </div>
+                        <H6 color="gray">Supplier</H6>
+                        <div data-trigger="Supplier" className="">
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Supplier List
+                                </a>
+                            </li>
+                            
+                        </div>
+                        <H6 color="gray">Accounting</H6>
+                        <div data-trigger="Accounting" className="">
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Cash Statement
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Balance Sheet
+                                </a>
+                            </li>
+                            <li className="px-4 rounded-lg mb-2 hover:bg-gray-300 ">
+                                <a
+                                    href="https://demos.creative-tim.com/material-tailwind-kit-react/#/landing"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-4 text-sm font-semibold py-3"
+                                >
+                                    <Icon name="web" size="2xl" />
+                                    Profit Loss Statement
+                                </a>
+                            </li>
+                            
+                        </div>
+                        
+                    </ul>
+
+                    
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default LeftSidebar
